fcd0e28b4895760ba41e50712108de04b1bc61ef
yngdrc <kheller7@gmail.com>
2025-03-01 19:13:31 +0100

    Initial commit

---
+++ app/src/main/java/app/aventurine/tibiabuddy/MainActivity.kt
@@ -0,0 +1,19 @@
+package app.aventurine.tibiabuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import app.aventurine.tibiabuddy.ui.theme.TibiaBuddyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TibiaBuddyTheme {
+
+            }
+        }
+    }
+}
